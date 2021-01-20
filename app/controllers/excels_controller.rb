6618b0fd21c3abf5a7b46a6eea0584d374d5424f
Tomoaki Shiomi <shiomi@glomouth.co.jp>
2021-01-20 15:15:05 +0900

    空のデータが送られた場合は新しくエクセルファイルを作成する

--- app/controllers/excels_controller.rb
+++ app/controllers/excels_controller.rb
@@ -4,8 +4,14 @@ class ExcelsController < ApplicationController
 
  def create
    file = params[:file]
-   workbook = RubyXL::Parser.parse file.path
-   workbook[0].add_cell 0, 0, 'changed'
-   send_data workbook.stream.string, type: file.content_type, filename: 'modified.xlsx'
+   if file
+    workbook = RubyXL::Parser.parse file.path
+    workbook[0].add_cell 0, 0, 'changed'
+    send_data workbook.stream.string, type: file.content_type, filename: 'modified.xlsx'
+  else
+    workbook = RubyXL::Workbook.new
+    workbook[0].add_cell 0, 0, 'changed'
+    send_data workbook.stream.string, type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", filename: 'modified.xlsx'
+   end
  end
 end
